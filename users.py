94891d416fa85c4a94a768fb04aea7fa204864ff
James Kariuki <karyorkir@gmail.com>
2017-10-17 00:10:24 +0300

    [feature] create a constructor for the global variables

--- users.py
+++ users.py
@@ -6,6 +6,9 @@ class Users(object):
     this class will handle all the functions related to the users
     """
 
+    def __init__(self, username=None, email=None, password=None):
+        """constructor to initialize the global variables"""
+
 
 
 
