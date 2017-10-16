45309bfba84e65ea865a00685285633b1702ee99
James Kariuki <karyorkir@gmail.com>
2017-10-16 19:20:47 +0300

    [feature] create route to the login page

--- app/views.py
+++ app/views.py
@@ -12,8 +12,14 @@ def register():
 
 
 
-    
     return render_template("register.html")
+
+@app.route("/login")
+def login():
+
+
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
