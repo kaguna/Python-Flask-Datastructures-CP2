b24d69bb7ed8300e104d458c4bd0470d701b4f01
James Kariuki <karyorkir@gmail.com>
2017-10-16 18:30:42 +0300

    [feature] create route to the register page

--- app/views.py
+++ app/views.py
@@ -7,5 +7,14 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/register")
+def register():
+
+
+
+    
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
