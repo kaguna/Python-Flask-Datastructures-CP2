7986a100c62c47b89ed3f9ec1e859fef56b02e82
James Kariuki <karyorkir@gmail.com>
2017-10-17 00:11:59 +0300

    [feature] add functionality to verify and validate user inputs
    This will check user inputs and verify against set conditions

--- users.py
+++ users.py
@@ -9,7 +9,30 @@ class Users(object):
     def __init__(self, username=None, email=None, password=None):
         """constructor to initialize the global variables"""
 
+        self.username=username
+        self.email=email
+        self.password=password
 
+    def register_user(self, username, email, password, cpassword):
+
+        regUsername = "[a-zA-Z0-9- .]"
+        regEmail = r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        regPassword = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+
+        if re.match(regUsername,username):
+            if username !=' ' and email !=' ' and password !=' ':
+                if username not in users.keys():
+                    if email not in users.keys():
+                        if password == cpassword:
+                            if re.search(regEmail, email):
+
+                                return 2
+                            return 3
+                        return 4
+                    return 5
+                return 6
+            return 7
+        return 8
 
 
 
