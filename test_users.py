bde8b46f25627c7050d193e8428dff436f2d18f4
James Kariuki <karyorkir@gmail.com>
2017-10-17 10:40:06 +0300

    [test] test the register user function in class Users

--- test_users.py
+++ test_users.py
@@ -1,4 +1,15 @@
 from unittest import TestCase
 from users import Users
 
+class TestUsers(TestCase):
+    """This class will handle all the functions to test for the user entry functions"""
+
+    def setUp(self):
+        """This method defines the test fixture for all test to be undertaken"""
+        self.new_user = Users()
+
+    def test_register_user(self):
+        """This will the function user_register"""
+        result = self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "password")
+        self.assertEqual(2,result,"User registration successful")
 
