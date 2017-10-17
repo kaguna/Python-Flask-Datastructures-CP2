3433d1d3c2cef4e136b1ebe76903140ff6148856
James Kariuki <karyorkir@gmail.com>
2017-10-17 22:27:33 +0300

    [feature] check whether confirm password field is empty

--- users.py
+++ users.py
@@ -20,7 +20,7 @@ class Users(object):
         regPassword = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
 
         if re.match(regUsername,username):
-            if username !=' ' and email !=' ' and password !=' ':
+            if username !=' ' and email !=' ' and password !=' ' and cpassword !=' ':
                 if username not in users.keys():
                     if email not in users.keys():
                         if password == cpassword:
