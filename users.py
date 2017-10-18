acedbef13468ed1234cbb4674fd0e20239c41b2c
James Kariuki <karyorkir@gmail.com>
2017-10-19 01:41:23 +0300

    [chore] refactor the code

--- users.py
+++ users.py
@@ -1,12 +1,13 @@
 import re
 users = {}
 
+
 class Users(object):
     """
     this class will handle all the functions related to the users
     """
 
-    def __init__(self, username = None, email = None, password = None):
+    def __init__(self, username=None, email=None, password=None):
         """constructor to initialize the global variables"""
 
         self.username = username
@@ -15,21 +16,28 @@ class Users(object):
 
     def register_user(self, username, email, password, cpassword):
 
-        regUsername = "[a-zA-Z0-9- .]"
-        regEmail = r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-        regPassword = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+        regusername = "[a-zA-Z0-9- .]"
+        regemail = r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        regpassword = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
 
-        if re.match(regUsername,username):
+        if re.match(regusername, username):
+
             if username != '' and email != '' and password != '' and cpassword != '' and cpassword != ' ':
+
                 if username not in users.keys():
+
                     if email not in users.keys():
+
                         if password == cpassword:
-                            if re.search(regEmail, email):
-                                if re.search(regPassword, password):
-                                    users[email]={ 'uname':username,
-                                                    'email':email,
-                                                    'password':password,
-                                                 }
+
+                            if re.search(regemail, email):
+
+                                if re.search(regpassword, password):
+
+                                    users[email] = {'uname': username,
+                                                    'email': email,
+                                                    'password': password,
+                                                    }
                                     return "dict_success"
                                 return "check_password_pattern"
                             return "check_email_pattern"
@@ -39,11 +47,9 @@ class Users(object):
             return "null_fields"
         return "check_username_pattern"
 
-
-
     def user_login(self, email, password):
         """This will help the user log in and access the resources"""
-        if email !='' and password != '':
+        if email != '' and password != '':
             if email in users.keys():
                 result = users[email]
                 result_password = result['password']
@@ -52,7 +58,3 @@ class Users(object):
                     return "check_match_password"
                 return "check_email_existence_in_dict"
             return "null_fields"
-
-
-
-
