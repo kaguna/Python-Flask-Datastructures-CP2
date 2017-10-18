2db2e24f7179df7bef52983f7f06d6592321f036
James Kariuki <karyorkir@gmail.com>
2017-10-19 01:30:45 +0300

    [fix] rename return values from numbers to descriptive word phrases

--- test_users.py
+++ test_users.py
@@ -12,35 +12,35 @@ class TestUsers(TestCase):
     def test_register_user(self):
         """This will the function user_register"""
         result = self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "password")
-        self.assertEqual(2, result, "User registration successful")
+        self.assertEqual("check_password_pattern", result, "User registration successful")
 
     def test_null_email_field(self):
         """ This will test whether the email field is null"""
         nullemailresult = self.new_user.register_user("kaguna", "", "password", "password")
-        self.assertEqual(7, nullemailresult, "Please fill the Email field.")
+        self.assertEqual("null_fields", nullemailresult, "Please fill the Email field.")
 
 
     def test_null_username_field(self):
         """ This will test whether the username field is null"""
         nullusernameresult = self.new_user.register_user("", "kaguna@gmail.com", "password", "password")
-        self.assertEqual(8,nullusernameresult,"Please fill the Username field.")
+        self.assertEqual("check_username_pattern",nullusernameresult,"Please fill the Username field.")
 
 
     def test_null_password_field(self):
         """ This will test whether the password field is null"""
         nullpasswordresult = self.new_user.register_user("kaguna", "kaguna@gmail.com", "", "password")
-        self.assertEqual(7, nullpasswordresult, "Please fill the password field.")
+        self.assertEqual("null_fields", nullpasswordresult, "Please fill the password field.")
 
 
     def test_null_cpassword_field(self):
         """ This will test whether the password field is null"""
         nullcpasswordresult = self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "")
-        self.assertEqual(7, nullcpasswordresult, "Please fill the confirm password field.")
+        self.assertEqual("null_fields", nullcpasswordresult, "Please fill the confirm password field.")
 
     def test_empty_cpassword_field(self):
         """ This will test whether the password field is empty"""
         emptycpasswordresult = self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", " ")
-        self.assertEqual(7, emptycpasswordresult, "Please fill the confirm password field.")
+        self.assertEqual("null_fields", emptycpasswordresult, "Please fill the confirm password field.")
 
 
 
@@ -48,7 +48,7 @@ class TestUsers(TestCase):
     def test_passwordEqualCpassword_field(self):
         """ This will test whether the password match the confirm password"""
         comparepasswordresult = self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "pass")
-        self.assertEqual(4, comparepasswordresult, "The passwords given do not match.")
+        self.assertEqual("match_passwords", comparepasswordresult, "The passwords given do not match.")
 
 
 
