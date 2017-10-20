401220f7f98e04d106d92d78bb54b57beb8e09d8
James Kariuki <karyorkir@gmail.com>
2017-10-20 14:34:44 +0300

    [feature] add functionality to test all login credentials under all conditions.

--- test_users.py
+++ test_users.py
@@ -9,8 +9,14 @@ class TestUsers(TestCase):
         """This method defines the test fixture for all test to be undertaken"""
         self.new_user = Users()
 
+        """
+        This will test all the registration credentials under all circumstances.
+        """
+
     def test_register_user(self):
-        """This will the function user_register"""
+        """Test registration when all inputs are stored in the dictionary."""
+        self.new_user.users = {}
+        self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "password")
         result = self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "password")
         self.assertEqual("check_password_pattern", result, "User registration successful")
 
@@ -19,18 +25,15 @@ class TestUsers(TestCase):
         nullemailresult = self.new_user.register_user("kaguna", "", "password", "password")
         self.assertEqual("null_fields", nullemailresult, "Please fill the Email field.")
 
-
     def test_null_username_field(self):
         """ This will test whether the username field is null"""
         nullusernameresult = self.new_user.register_user("", "kaguna@gmail.com", "password", "password")
-        self.assertEqual("check_username_pattern",nullusernameresult,"Please fill the Username field.")
-
+        self.assertEqual("check_username_pattern", nullusernameresult, "Please fill the Username field.")
 
     def test_null_password_field(self):
         """ This will test whether the password field is null"""
         nullpasswordresult = self.new_user.register_user("kaguna", "kaguna@gmail.com", "", "password")
         self.assertEqual("null_fields", nullpasswordresult, "Please fill the password field.")
-
 
     def test_null_cpassword_field(self):
         """ This will test whether the password field is null"""
@@ -42,13 +45,56 @@ class TestUsers(TestCase):
         emptycpasswordresult = self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", " ")
         self.assertEqual("null_fields", emptycpasswordresult, "Please fill the confirm password field.")
 
-
-
-
     def test_passwordEqualCpassword_field(self):
         """ This will test whether the password match the confirm password"""
         comparepasswordresult = self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "pass")
         self.assertEqual("match_passwords", comparepasswordresult, "The passwords given do not match.")
 
+        """
+        This will test the login credentials under all given conditions.
+        """
+    def test_login_success(self):
+        """ Test the inputs required for a valid login"""
+        self.new_user.users = {}
+        self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "password")
+        validlogin= self.new_user.user_login("kaguna@gmail.com", "password")
+        self.assertEqual("check_email_password_dict", validlogin, "Login successful.")
 
+    def test_login_match_password(self):
+        """ Test login when the password differs with the one in the dictionary.."""
+        self.new_user.users = {}
+        self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "password")
+        invalidmatchpassword = self.new_user.user_login("kaguna@gmail.com", "pass")
+        self.assertEqual("check_email_password_dict", invalidmatchpassword, "Incorrect password given.")
 
+    def test_login_match_email(self):
+        """ Test login when the email differs with the one in the dictionary."""
+        self.new_user.users = {}
+        self.new_user.register_user("kaguna", "kaguna@gmail.com", "password", "password")
+        invalidmatchpassword = self.new_user.user_login("jimmy@gmail.com", "password")
+        self.assertEqual("check_email_password_dict", invalidmatchpassword, "Incorrect password given.")
+
+    def test_login_inputnull(self):
+        """ Test login when all inputs are null"""
+        nullinputs = self.new_user.user_login("", "")
+        self.assertEqual("null_empty_fields", nullinputs, "Please fill all the fields.")
+
+    def test_login_nullemail(self):
+        """ Test login when email input is null"""
+        nullemail = self.new_user.user_login("", "password")
+        self.assertEqual("null_empty_fields", nullemail, "Please fill the email field.")
+
+    def test_login_nullpassword(self):
+        """ Test login when password input is null"""
+        nullpassword = self.new_user.user_login("kaguna@gmail.com", "")
+        self.assertEqual("null_empty_fields", nullpassword, "Please fill the password field.")
+
+    def test_login_emptypassword(self):
+        """ Test login when password input is empty"""
+        emptypassword = self.new_user.user_login("kaguna@gmail.com", "  ")
+        self.assertEqual("null_empty_fields", emptypassword, "Password field should not be empty.")
+
+    def test_login_emptyemail(self):
+        """ Test login when password input is empty"""
+        emptyemail = self.new_user.user_login("  ", "password")
+        self.assertEqual("null_empty_fields", emptyemail, "Email field should not be empty.")
