ae2b52e9714f512b2e1d49e3055b941a8cd28914
James Kariuki <karyorkir@gmail.com>
2017-10-21 19:45:18 +0300

    [Refactor] remove random generated number not in use

--- categories.py
+++ categories.py
@@ -1,4 +1,4 @@
-import re, random
+import re
 class Categories(object):
     """
     this class will handle all the functions related to the users
@@ -14,7 +14,6 @@ class Categories(object):
     def create_category(self, category_name, category_owner):
 
         regexcategory_name = "[a-zA-Z0-9- .]"
-        categoryId = random.choice(range(1, 100))
 
         if re.match(regexcategory_name, category_name):
 
@@ -22,13 +21,10 @@ class Categories(object):
 
                 if category_name not in self.categories.keys():
 
-                    if categoryId not in self.categories.keys():
-                        self.categories[category_name] = {'cat_id': categoryId,
-                                                    'cat_name': category_name,
-                                                    'cat_owner': category_owner,
-                                                    }
+                        self.categories[category_name] = {'cat_name': category_name,
+                                                           'cat_owner': category_owner,
+                                                         }
                         return "success"
-                    return "catid_uniqueness"
                 return "catname_uniqueness"
             return "null_empty_field"
         return "catname_pattern"
