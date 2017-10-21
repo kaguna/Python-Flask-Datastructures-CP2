b07174069753b2facc86eeeffceddb4bd05b6048
James Kariuki <karyorkir@gmail.com>
2017-10-22 02:35:54 +0300

    [feature] add strip to check for white spaces and pep8 refactoring

--- categories.py
+++ categories.py
@@ -1,11 +1,13 @@
 import re
+
+
 class Categories(object):
     """
     this class will handle all the functions related to the users
     """
     categories = {}
 
-    def __init__(self,category_name=None, category_owner=None):
+    def __init__(self, category_name=None, category_owner=None):
         """constructor to initialize the global variables"""
 
         self.category_name = category_name
@@ -17,17 +19,14 @@ class Categories(object):
 
         if re.match(regexcategory_name, category_name):
 
-            if category_name != '' and category_name != ' ':
+            if category_name != '' and category_name.strip():
 
                 if category_name not in self.categories.keys():
 
                         self.categories[category_name] = {'cat_name': category_name,
-                                                           'cat_owner': category_owner,
-                                                         }
+                                                          'cat_owner': category_owner,
+                                                          }
                         return "success"
                 return "catname_uniqueness"
             return "null_empty_field"
         return "catname_pattern"
-
-
-
