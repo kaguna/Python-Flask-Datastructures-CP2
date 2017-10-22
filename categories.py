3c721fe68fc31409c30c1182f011e6fcca6f2d7c
James Kariuki <karyorkir@gmail.com>
2017-10-22 16:08:42 +0300

    [Feature] add an empty dictionary and refactoring data variable to cats

--- categories.py
+++ categories.py
@@ -6,6 +6,7 @@ class Categories(object):
     this class will handle all the functions related to the users
     """
     categories = {}
+    recipes = {}
 
     def __init__(self, category_name=None, category_owner=None):
         """constructor to initialize the global variables"""
@@ -31,10 +32,38 @@ class Categories(object):
             return "null_empty_field"
         return "catname_pattern"
 
-    def view_category(self,category_owner):
-        data = self.categories
+    def view_category(self, category_owner):
+        cats = self.categories
         my_category = []
-        for category in data:
-            if data[category]['cat_owner'] == category_owner:
+        for category in cats:
+            if cats[category]['cat_owner'] == category_owner:
                my_category.append(category)
-        return   my_category
+        return my_category
+
+    def create_recipe(self, recipe_name, category_name, category_owner):
+
+     regexrecipe_name = "[a-zA-Z0-9- .]"
+
+     if re.match(regexrecipe_name, recipe_name):
+
+         if recipe_name != '' and recipe_name.strip():
+
+             if recipe_name not in self.recipes.keys():
+
+                     self.recipes[recipe_name] = {'recipe_name': recipe_name,
+                                                  'cat_name': category_name,
+                                                  'cat_owner': category_owner,
+                                                  }
+                     return "success"
+             return "recipename_uniqueness"
+         return "null_empty_field"
+     return "recipename_pattern"
+
+
+    def view_recipes(self, category_name):
+        data = self.recipes
+        my_recipes = []
+        for recipe in data:
+            if data[recipe]['cat_name'] == category_name:
+               my_recipes.append(recipe)
+        return my_recipes
