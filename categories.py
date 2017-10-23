4df79665171069a44f1b587d08c1dc41b8a3f395
James Kariuki <karyorkir@gmail.com>
2017-10-23 04:16:18 +0300

    [Refactor] arranges the lines of codes

--- categories.py
+++ categories.py
@@ -15,7 +15,7 @@ class Categories(object):
         self.category_owner = category_owner
 
     def create_category(self, category_name, category_owner):
-
+        """This will create new category"""
         regexcategory_name = "[a-zA-Z0-9- .]"
 
         if re.match(regexcategory_name, category_name):
@@ -33,6 +33,7 @@ class Categories(object):
         return "catname_pattern"
 
     def view_category(self, category_owner):
+        """This will display the categories"""
         cats = self.categories
         my_category = []
         for category in cats:
@@ -41,23 +42,19 @@ class Categories(object):
         return my_category
 
     def create_recipe(self, recipe_name, category_name, category_owner):
-
-     regexrecipe_name = "[a-zA-Z0-9- .]"
-
-     if re.match(regexrecipe_name, recipe_name):
-
-         if recipe_name != '' and recipe_name.strip():
-
-             if recipe_name not in self.recipes.keys():
-
-                     self.recipes[recipe_name] = {'recipe_name': recipe_name,
+        """This will add a recipe to dictionary"""
+        regexrecipe_name = "[a-zA-Z0-9- .]"
+        if re.match(regexrecipe_name, recipe_name):
+            if recipe_name != '' and recipe_name.strip():
+                if recipe_name not in self.recipes.keys():
+                    self.recipes[recipe_name] = {'recipe_name': recipe_name,
                                                   'cat_name': category_name,
                                                   'cat_owner': category_owner,
-                                                  }
-                     return "success"
-             return "recipename_uniqueness"
-         return "null_empty_field"
-     return "recipename_pattern"
+                                                }
+                    return "success"
+                return "recipename_uniqueness"
+            return "null_empty_field"
+        return "recipename_pattern"
 
 
     def view_recipes(self, category_name):
@@ -90,13 +87,15 @@ class Categories(object):
 
 
     def delete_category(self,current_name):
-         list_of_categories = self.categories
-         if list_of_categories[current_name]["cat_name"] == current_name:
-             del list_of_categories[current_name]
-         return list_of_categories
+        """This will delete the categories"""
+        list_of_categories = self.categories
+        if list_of_categories[current_name]["cat_name"] == current_name:
+            del list_of_categories[current_name]
+        return list_of_categories
 
     def delete_recipe(self,current_name):
-         list_of_recipes = self.recipes
-         if list_of_recipes[current_name]["recipe_name"] == current_name:
-             del list_of_recipes[current_name]
-         return list_of_recipes
+        """This will delete the recipes"""
+        list_of_recipes = self.recipes
+        if list_of_recipes[current_name]["recipe_name"] == current_name:
+            del list_of_recipes[current_name]
+        return list_of_recipes
