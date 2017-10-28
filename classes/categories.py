9c6678b79983201fcdc18bdbba3735dd13af3c7b
James Kariuki <karyorkir@gmail.com>
2017-10-28 23:55:32 +0300

    [fix] delete recipes using the function

--- classes/categories.py
+++ classes/categories.py
@@ -42,6 +42,38 @@ class Categories(object):
         personal_list = [owner_list for owner_list in self.categories if category_owner in owner_list]
         return personal_list
 
+    def edit_category(self, current_name, new_name, category_owner):
+        """Update the category name"""
+        personal_categories = [owner_list for owner_list in self.categories
+                               if category_owner in owner_list]
+
+        similar_cat_list = [searched_cat_name for searched_cat_name in personal_categories
+                            if searched_cat_name[0] == new_name]
+
+        regexcategory_name = "[a-zA-Z0-9- .]"
+        if re.match(regexcategory_name, new_name):
+            if new_name != '' and new_name.strip():
+                for catList in personal_categories:
+                    if current_name in catList:
+                        if similar_cat_list == []:
+                            category_name_index = personal_categories.index(catList)
+                            personal_categories[category_name_index][0] = new_name
+                            return "success"
+                        return "categoryname_uniqueness"
+            return "null_empty_field"
+        return "categoryname_pattern"
+
+    def delete_category(self, category_name, category_owner):
+        """This will delete the categories from user in session"""
+        personal_categories = [owner_list for owner_list in self.categories
+                               if category_owner in owner_list]
+        for catList in personal_categories:
+            if category_name in catList:
+               category_list_index = personal_categories.index(catList)
+               del self.categories[category_list_index]
+               del personal_categories[category_list_index]
+        return personal_categories
+
     def create_recipe(self, recipe_name, category_name, category_owner):
         """This will add a recipe to the list"""
         specific_category_recipes = [recipes for recipes in self.recipes
@@ -71,46 +103,35 @@ class Categories(object):
                                      if recipe_owner == recipes[2] and category_name == recipes[1]]
         return specific_category_recipes
 
-
-    def edit_category(self, current_name, new_name, category_owner):
+    def edit_recipe(self, current_recipe_name, new_recipe_name, category_name, recipe_owner):
         """Update the category name"""
-        personal_categories = [owner_list for owner_list in self.categories
-                               if category_owner in owner_list]
+        specific_category_recipes = [recipes for recipes in self.recipes
+                                     if recipe_owner == recipes[2] and category_name == recipes[1]]
 
-        similar_cat_list = [searched_cat_name for searched_cat_name in personal_categories
-                            if searched_cat_name[0] == new_name]
+        similar_recipe_list = [searched_recipe_name for searched_recipe_name in specific_category_recipes
+                            if searched_recipe_name[0] == new_recipe_name]
 
-        regexcategory_name = "[a-zA-Z0-9- .]"
-        if re.match(regexcategory_name, new_name):
-            if new_name != '' and new_name.strip():
-                for catList in personal_categories:
-                    if current_name in catList:
-                        if similar_cat_list == []:
-                            category_name_index = personal_categories.index(catList)
-                            personal_categories[category_name_index][0] = new_name
+        regexrecipe_name = "[a-zA-Z0-9- .]"
+        if re.match(regexrecipe_name, new_recipe_name):
+            if new_recipe_name != '' and new_recipe_name.strip():
+                for recList in specific_category_recipes:
+                    if current_recipe_name in recList:
+                        if similar_recipe_list == []:
+                            recipe_name_index = specific_category_recipes.index(recList)
+                            specific_category_recipes[recipe_name_index][0] = new_recipe_name
                             return "success"
-                        return "categoryname_uniqueness"
+                        return "recipename_uniqueness"
             return "null_empty_field"
-        return "categoryname_pattern"
+        return "recipename_pattern"
 
 
-    def delete_category(self, category_name, category_owner):
-        """This will delete the categories from user in session"""
-        personal_categories = [owner_list for owner_list in self.categories
-                               if category_owner in owner_list]
-        for catList in personal_categories:
-            if category_name in catList:
-               category_list_index = personal_categories.index(catList)
-               del self.categories[category_list_index]
-               del personal_categories[category_list_index]
-        return personal_categories
-
-    def delete_recipe(self,current_name):
+    def delete_recipe(self, recipe_name, category_name, recipe_owner):
         """This will delete the recipes"""
-        list_of_recipes = self.recipes
-        if current_name in list_of_recipes.keys():
-            if list_of_recipes[current_name]["recipe_name"] == current_name:
-                del list_of_recipes[current_name]
-                return list_of_recipes
-            return list_of_recipes
-        return list_of_recipes
+        specific_category_recipes = [recipes for recipes in self.recipes
+                                     if recipe_owner == recipes[2] and category_name == recipes[1]]
+        for recList in specific_category_recipes:
+            if recipe_name in recList:
+                recipe_list_index = specific_category_recipes.index(recList)
+                del self.recipes[recipe_list_index]
+                del specific_category_recipes[recipe_list_index]
+        return specific_category_recipes
