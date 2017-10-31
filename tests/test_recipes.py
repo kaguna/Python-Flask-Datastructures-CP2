f429df3147dfdbdf0504616500a637dd91076417
James Kariuki <karyorkir@gmail.com>
2017-10-31 23:55:41 +0300

    [fix] add the procedure parameter to the function

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -14,31 +14,31 @@ class TestRecipes(TestCase):
         """
     def test_empty_recipename(self):
         """Test if recipe name is empty"""
-        emptyfield = self.new_category.create_recipe(" ", "Lunch", "kaguna@gmail.com")
+        emptyfield = self.new_category.create_recipe(" ", "Lunch", "add description of recipe", "kaguna@gmail.com")
         self.assertEqual("null_empty_field", emptyfield, "Please give the recipe name.")
 
     def test_null_recipename(self):
         """Test if recipe name is null"""
-        nullfield= self.new_category.create_recipe(" ", "Lunch", "kaguna@gmail.com" )
+        nullfield= self.new_category.create_recipe(" ", "Lunch", "add description of recipe", "kaguna@gmail.com" )
         self.assertEqual("null_empty_field", nullfield, "Please give the recipe name.")
 
     def test_pattern_recipename(self):
         """Test if pattern name follows the pattern given"""
-        recnamepattern =  self.new_category.create_recipe("#^", "lunch","kaguna@gmail.com" )
+        recnamepattern =  self.new_category.create_recipe("#^", "lunch", "add description of recipe","kaguna@gmail.com" )
         self.assertEqual("recipename_pattern", recnamepattern, "Invalid category name.")
 
     def test_recipename_exist(self):
-        """Test if recipe name exists in the dictionary"""
+        """Test if recipe name exists in the recipe list"""
         self.new_category.recipes = []
-        self.new_category.create_recipe("sembe","lunch", "kaguna@gmail.com")
-        recipenameexist = self.new_category.create_recipe("sembe","lunch", "kaguna@gmail.com")
+        self.new_category.create_recipe("sembe","lunch", "add description of recipe", "kaguna@gmail.com")
+        recipenameexist = self.new_category.create_recipe("sembe","lunch", "add description of recipe", "kaguna@gmail.com")
         self.assertEqual("recipename_uniqueness", recipenameexist, "Recipe name exists.")
 
     def test_recipename_create(self):
         """Test if recipe name is different and register"""
         self.new_category.recipes = []
-        self.new_category.create_recipe("Githeri","lunch", "kaguna@gmail.com")
-        create_recname = self.new_category.create_recipe("ugali","supper", "kaguna@gmail.com")
+        self.new_category.create_recipe("Githeri","lunch", "add description of recipe", "kaguna@gmail.com")
+        create_recname = self.new_category.create_recipe("ugali","supper", "add description of recipe", "kaguna@gmail.com")
         self.assertEqual("success", create_recname, "Recipe created successfully.")
 
 
