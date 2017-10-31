7c38cb34c4c5a35bcee69a14d3a7d7316ec09d4b
James Kariuki <karyorkir@gmail.com>
2017-11-01 00:47:05 +0300

    [Chore] add tests for updating the recipe name

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -10,12 +10,17 @@ class TestRecipes(TestCase):
         self.new_category = Categories()
 
         """
-        This will test recipe name entry under all circumstances.
+        This will test recipe name entry under all circumstances when creating a recipe.
         """
     def test_empty_recipename(self):
-        """Test if recipe name is empty"""
+        """Test on the messge poped upon registering empty recipe name"""
         emptyfield = self.new_category.create_recipe(" ", "Lunch", "add description of recipe", "kaguna@gmail.com")
         self.assertEqual("null_empty_field", emptyfield, "Please give the recipe name.")
+
+    def test_empty_procedure(self):
+        """Test on the messge poped upon registering empty recipe procedure"""
+        emptyfield = self.new_category.create_recipe("sembe", "Lunch", " ", "kaguna@gmail.com")
+        self.assertEqual("null_empty_field", emptyfield, "Please give the recipe procedure.")
 
     def test_null_recipename(self):
         """Test if recipe name is null"""
@@ -25,7 +30,7 @@ class TestRecipes(TestCase):
     def test_pattern_recipename(self):
         """Test if pattern name follows the pattern given"""
         recnamepattern =  self.new_category.create_recipe("#^", "lunch", "add description of recipe","kaguna@gmail.com" )
-        self.assertEqual("recipename_pattern", recnamepattern, "Invalid category name.")
+        self.assertEqual("recipename_pattern", recnamepattern, "Invalid recipe name.")
 
     def test_recipename_exist(self):
         """Test if recipe name exists in the recipe list"""
@@ -40,5 +45,28 @@ class TestRecipes(TestCase):
         self.new_category.create_recipe("Githeri","lunch", "add description of recipe", "kaguna@gmail.com")
         create_recname = self.new_category.create_recipe("ugali","supper", "add description of recipe", "kaguna@gmail.com")
         self.assertEqual("success", create_recname, "Recipe created successfully.")
+
+        """
+           This will test recipe name entry when updating a recipe.
+           """
+
+    # def test_update_pattern_recipename(self):
+    #     """Test if new recipe name follows the pattern given"""
+    #     recnamepattern = self.new_category.create_recipe("ugali", '@@###', "Lunch", "kaguna@gmail.com")
+    #     self.assertEqual("recipename_pattern", recnamepattern, "Invalid new recipe name.")
+
+    def test_update_recipename_exist(self):
+        """Test if the new recipe name exists in the recipe list"""
+        self.new_category.recipes = []
+        self.new_category.create_recipe("ugali", "sembe", "Lunch", "kaguna@gmail.com")
+        recipenameexist = self.new_category.create_recipe("ugali", "sembe", "Lunch", "kaguna@gmail.com")
+        self.assertEqual("recipename_uniqueness", recipenameexist, "Recipe name exists.")
+
+    def test_recipename_update(self):
+        """Test if recipe name is unique and register"""
+        self.new_category.recipes = []
+        self.new_category.create_recipe("ugali", "Sembe", "Lunch", "kaguna@gmail.com")
+        create_recname = self.new_category.create_recipe("ugali", "ngima", "Lunch", "kaguna@gmail.com")
+        self.assertEqual("success", create_recname, "Recipe updated successfully.")
 
 
